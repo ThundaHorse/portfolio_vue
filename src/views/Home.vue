6a87555dafd7b5eb9da3fcc52d4aa441cfe72941
ThundaHorse <abeeeeekim@gmail.com>
2019-08-09 14:30:10 -0400

    transferring to html template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,36 @@
 <template>
   <div id="home">
     <!-- height: {{ window.height }}
-    width: {{ window.width }} -->
+    width: {{ window.width }}-->
     <div v-if="window.width > 850 || window.height > 1000">
       <NonMobileViewing></NonMobileViewing>
+      <!-- <Formatted /> -->
+      <!-- <Footer /> -->
     </div>
     <div v-else>
       <MobileMin></MobileMin>
+      <!-- <Formatted /> -->
+      <!-- <Footer /> -->
     </div>
   </div>
 </template>
 
 <style>
- 
 </style>
 
 <script>
-import NonMobileViewing from '../components/NonMobileViewing';
+import NonMobileViewing from "../components/NonMobileViewing";
 // import MobileViewing from '../components/MobileViewing';
-import MobileMin from '../components/MobileMin';
+import MobileMin from "../components/MobileMin";
+// import Footer from "../components/Footer";
+// import Formatted from "../components/Formatted";
 
 export default {
   name: "Home",
   components: {
     NonMobileViewing,
+    // Footer,
+    // Formatted
     // MobileViewing,
     MobileMin
   },
@@ -33,14 +40,14 @@ export default {
         width: 0,
         height: 0
       }
-    }
+    };
   },
   created: function() {
-    window.addEventListener('resize', this.handleResize)
+    window.addEventListener("resize", this.handleResize);
     this.handleResize();
   },
   destroyed() {
-    window.removeEventListener('resize', this.handleResize)
+    window.removeEventListener("resize", this.handleResize);
   },
   methods: {
     handleResize() {
